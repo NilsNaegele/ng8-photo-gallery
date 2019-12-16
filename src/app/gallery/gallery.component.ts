1e6e558b7251725de7e5f2bb0403bae3413358f9
NilsNaegele <nils.holger.naegele@gmail.com>
2019-12-16 05:06:16 +0100

    sidebar cmp

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+
+import { ImageService } from '../shared/image.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnChanges {
 
-  constructor() { }
+  constructor(private imageService: ImageService) {
+      this.visibleImages = this.imageService.getImages();
+  }
+  title = 'Auf Bild clicken für ganze Größe';
+  @Input() filterBy ? = 'alle';
 
-  ngOnInit() {
+  visibleImages: any[] = [];
+
+  private toggle = false;
+
+  classes: string[] = [];
+  show = false;
+
+  ngOnChanges() {
+      this.visibleImages = this.imageService.getImages();
+  }
+
+  clickEvent() {
+      console.log('sidebarToggled');
+      this.classes.splice(this.classes.length - 1, 1);
+      this.show = !this.show;
+      if (this.show) {
+          this.classes.push('active1');
+      } else {
+          this.classes.push('not-active1');
+      }
   }
 
 }
