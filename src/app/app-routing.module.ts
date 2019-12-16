bfa8d968852d5134ba856f625b95d186808eda4f
NilsNaegele <nils.holger.naegele@gmail.com>
2019-12-16 04:25:54 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { GalleryComponent } from './gallery/gallery.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'gallerie', component: GalleryComponent},
+  { path: '', redirectTo: 'gallerie', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
